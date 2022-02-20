b49dcd0b77eafb26baf4dbb8fb39b82b07dd6d95
Jeff Myers II <jeffrey.myers648@topper.wku.edu>
2022-02-19 19:29:38 -0600

    Added password hashing and login endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,26 @@
 const { Router } = require('express')
 const { connect, query } = require('../helpers/mysql.helper')
+const crypto = require('crypto')
 
 const router = Router()
 
+router.post('/login', (req, res, next) => {
+  const { user_nickname, user_password } = req.body
+  const hash = crypto.createHash('sha256').update(user_password).digest('hex')
+
+  const con = connect()
+  const sql = `select user_id, user_creation_date, user_update_date, quiz_time_limit, quiz_question_limit from users where user_nickname='${user_nickname}' and user_password='${hash}'`
+  query({ con, sql, res })
+})
+
 router.get('/get', (req, res, next) => {
   const { user_id } = req.query
+
+  if(user_id === undefined) {
+    res.send({ success: false, error: 'Query parameters must contain user_id.' })
+    return
+  }
+
   const con = connect()
   const sql = `select user_nickname, user_creation_date, user_update_date, quiz_time_limit, quiz_question_limit from users where user_id=${user_id}`
   query({ con, sql, res })
@@ -12,6 +28,12 @@ router.get('/get', (req, res, next) => {
 
 router.get('/get/name', (req, res, next) => {
   const { user_nickname } = req.query
+
+  if(user_nickname === undefined) {
+    res.send({ success: false, error: 'Query parameters must contain user_nickname.' })
+    return
+  }
+
   const con = connect()
   const sql = `select user_id, user_creation_date, user_update_date, quiz_time_limit, quiz_question_limit from users where user_nickname='${user_nickname}'`
   query({ con, sql, res })
@@ -19,13 +41,27 @@ router.get('/get/name', (req, res, next) => {
 
 router.post('/insert', (req, res, next) => {
   const { user_nickname, user_password } = req.body
+  
+  if(user_nickname === undefined || user_password === undefined) {
+    res.send({ success: false, error: 'Body must contain user_nickname and user_password.' })
+    return
+  }
+
+  const hash = crypto.createHash('sha256').update(user_password).digest('hex')
+
   const con = connect()
-  const sql = `insert into users (user_nickname, user_password) values ('${user_nickname}', '${user_password}')`
+  const sql = `insert into users (user_nickname, user_password) values ('${user_nickname}', '${hash}')`
   query({ con, sql, res })
 })
 
 router.post('/update', (req, res, next) => {
   const { user_id, user_password, quiz_time_limit, quiz_question_limit } = req.body
+
+  if(user_id === undefined) {
+    res.send({ success: false, error: 'Body must contain user_id.' })
+    return
+  }
+
   const user_update_date = Date.now()
   const con = connect()
 
