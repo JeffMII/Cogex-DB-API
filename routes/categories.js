8cd854a8a827a95ad4a762625821c8934ed4a518
Jeff Myers II <jeffrey.myers648@topper.wku.edu>
2022-02-22 20:42:53 -0600

    Fixed categories route

--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,11 @@
 const { Router } = require('express')
-const { connect, query } = require('../helpers/mysql.helper')
+const { query } = require('../helpers/mysql.helper')
 
 const router = Router()
 
 router.get('/get', (req, res, next) => {
-  const con = connect()
   const sql = `select sup.super_category_id, sup.super_category, sup.super_category_display, sub.sub_category_id, sub.sub_category, sub.sub_category_display from super_categories sup left join sub_categories sub on sup.super_category_id=sub.super_category_id order by sup.super_category_id`
-  query({ con, sql, res })
+  query({ sql, res })
 })
 
 module.exports = router
